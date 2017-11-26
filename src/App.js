4104fa74d48cbc3a4a05b86fddfbd9a4fdaffa6f
Lesley Park <lesparkley@gmail.com>
2017-11-26 06:04:01 +0000

    Close tag form after submission

--- src/App.js
+++ src/App.js
@@ -61,6 +61,9 @@ class App extends Component {
     this.setState({ newTag: true });
   }
 
+  closeTagForm = () => {
+    this.setState({ newTag: false });
+  }
 
   render() {
     const { showNote, notes, note, newTag } = this.state;
@@ -73,6 +76,7 @@ class App extends Component {
             submitNote={this.submitNote}
             showTagForm={this.showTagForm}
             newTag={newTag}
+            closeTagForm={this.closeTagForm}
           /> 
           : 
           <List 
